13129549a81e9630aab6d6d57bd95abcc39439bd
0x580x540x43 <78674390+0x580x540x43@users.noreply.github.com>
2022-12-25 11:24:42 -0500

    Update Loadstring.lua

--- Loadstring.lua
+++ Loadstring.lua
@@ -1,2 +1,1 @@
-local Creator, Discord = "Moon", "discord.gg/Nxj2nh9YJD"
-loadstring(game:HttpGet("https://raw.githubusercontent.com/0x580x540x43/Animator/main/Main.lua"))(Creator, Discord)
+loadstring(game:HttpGet("https://raw.githubusercontent.com/0x580x540x43/Animator/main/Main.lua"))()
